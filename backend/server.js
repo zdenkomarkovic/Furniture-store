9c51d471e99611161b33cb91f15060558c562858
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-16 01:50:39 +0100

    a

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,10 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10mb' }));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Furniture store - its work');
+});
+
 app.use('/', require('./routes'));
 
 app.listen(PORT, () => {
