5f8747870daca409d5968c2b4e12b4f1c31e9c78
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-17 23:49:20 +0100

    vbn

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { useEffect } from 'react';
 import Hero from '../../Components/Hero/Hero';
-import UserService from '../../services/userService';
 
 const Home = () => {
-  useEffect(() => {
-    UserService.getUser('zdenko.staff@gmail.com').then(res => {
-      console.log(res.data);
-    });
-  }, []);
   return (
     <div>
       <Hero />
