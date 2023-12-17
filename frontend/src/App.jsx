48ee44b09d9cf1b8434f6c49d70c6aa9c97403b4
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-17 23:34:29 +0100

    w

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import axios from 'axios';
-import UserService from './services/userService';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Cart from './Components/Cart/Cart';
