7e1048435207fd1585518cff4abdb5c6822a0510
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-22 00:32:10 +0100

    css

--- frontend/src/Pages/Products/Products.jsx
+++ frontend/src/Pages/Products/Products.jsx
@@ -1,25 +1,25 @@
-import { motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import Header from '../../Components/Header/Header';
-import ProductService from '../../services/ProductService';
-import { Link } from 'react-router-dom';
-import './Products.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { storeAllProducts } from '../../store/productSlice';
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import Header from "../../Components/Header/Header";
+import ProductService from "../../services/ProductService";
+import { Link } from "react-router-dom";
+import "./Products.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { storeAllProducts } from "../../store/productSlice";
 
 const Products = () => {
   const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
-  const { products } = useSelector(state => state.productStore);
+  const { products } = useSelector((state) => state.productStore);
 
   useEffect(() => {
     ProductService.getAllProducts()
-      .then(res => {
+      .then((res) => {
         dispatch(storeAllProducts(res.data));
 
         setIsLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         setIsLoading(false);
       });
@@ -27,43 +27,42 @@ const Products = () => {
 
   return (
     <>
-      <Header title='Products' />
-      <div className='container products-wrapper'>
+      <Header title="Products" />
+      <div className="container products-wrapper">
         {isLoading ? (
-          <div className='products'>
+          <div className="products">
             {Array.from({ length: 6 }).map((_, id) => (
-              <motion.div key={id} className='card skeleton'></motion.div>
+              <motion.div key={id} className="card skeleton"></motion.div>
             ))}
           </div>
         ) : (
-          <div className='products'>
+          <div className="products">
             {products.map((product, i) => (
-              <Link to={`/single/${product._id}`} key={i}>
-                <motion.div
-                  whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-                  transition={{ duration: 0.5 }}
-                  className='card'
-                >
-                  <div className='image'>
-                    <img src={product.thumbnail} alt={product.name} />
-                  </div>
-                  <div className='text'>
-                    <h6>{product.title}</h6>
-                    <p>$ {product.price.toLocaleString()}</p>
-                    <motion.button
-                      whileHover={{ scale: 1.1 }}
-                      transition={{
-                        type: 'spring',
-                        stiffness: 400,
-                        damping: 10,
-                      }}
-                      className='product-btn'
-                    >
-                      Details
-                    </motion.button>
-                  </div>
-                </motion.div>
-              </Link>
+              <motion.div
+                whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="card"
+                key={i}
+              >
+                <div className="image">
+                  <img src={product.thumbnail} alt={product.name} />
+                </div>
+                <div className="text">
+                  <h6>{product.title}</h6>
+                  <p>$ {product.price.toLocaleString()}</p>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 400,
+                      damping: 10,
+                    }}
+                    className="product-btn"
+                  >
+                    <Link to={`/single/${product._id}`}> Details </Link>
+                  </motion.button>
+                </div>
+              </motion.div>
             ))}
           </div>
         )}
