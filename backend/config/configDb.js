b392d88a5ed5748c69fc241cea3ec8d18a474a7d
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-23 21:45:28 +0100

    d

--- backend/config/configDb.js
+++ backend/config/configDb.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
-const whiteList = ["http://localhost:63342", "http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://furniture-store-r4p8.vercel.app",
+];
 
 module.exports = {
   JWT_KEY: process.env.JWT_KEY,
   DB_URL: process.env.DB_URL,
   PORT: process.env.PORT,
   CORS_OPTIONS: {
-    origin: (origin, cb) => {
-      if (whiteList.includes(origin)) {
-        cb(null, true);
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true)
       } else {
-        cb(new Error("Not allowed by CORS"));
+        callback(new Error('Not allowed by CORS'))
       }
-      cb(null, true);
-    },
-  },
-};
+    }
+  }
