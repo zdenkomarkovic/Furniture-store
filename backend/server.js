7484b5572a75ccbb574d965ed66debc7146010ad
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-24 18:02:34 +0100

    j

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.send(process.env);
