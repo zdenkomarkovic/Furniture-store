f363a4ce03ed7feb9e249d17e19f5302cecf2582
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-24 20:53:44 +0100

    responsive

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,65 +1,69 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Header from '../../Components/Header/Header';
-import { LS_TOKEN } from '../../config/config';
-import { routes } from '../../router/routes';
-import UserService from '../../services/UserService';
-import { setUser } from '../../store/userSlice';
-import './Login.scss';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Header from "../../Components/Header/Header";
+import { LS_TOKEN } from "../../config/config";
+import { routes } from "../../router/routes";
+import UserService from "../../services/UserService";
+import { setUser } from "../../store/userSlice";
+import "./Login.scss";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const dispatch = useDispatch();
   const [inputData, setInputData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const navigate = useNavigate();
-  const handleInput = e => {
+  const handleInput = (e) => {
     setInputData({ ...inputData, [e.target.name]: e.target.value });
   };
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     UserService.login(inputData)
-      .then(res => {
+      .then((res) => {
         if (res.status === 201) {
         } else {
           dispatch(setUser(res.data.user));
           localStorage.setItem(LS_TOKEN, res.data.token);
-          toast.success('You are logged successfully');
+          toast.success("You are logged successfully");
           setTimeout(() => {
             navigate(routes.DASHBOARD.path);
           }, 2000);
         }
       })
-      .catch(err => {
-        console.log('Error');
+      .catch((err) => {
+        console.log("Error");
         console.log(err);
       });
   };
   return (
     <div>
-      <Header title='Login' />
-      <div className='container'>
-        <div className='form-input'>
-          <form action='' onSubmit={handleSubmit}>
-            <input
-              type='email'
-              name='email'
-              placeholder='Enter email...'
-              value={inputData.email}
-              onChange={handleInput}
-            />
-            <input
-              type='password'
-              name='password'
-              placeholder='Enter password...'
-              value={inputData.password}
-              onChange={handleInput}
-            />
-            <button>Login</button>
-          </form>
+      <Header title="Login" />
+      <div className="login-wrapper">
+        <div className="container">
+          <div className="form-input">
+            <form action="" onSubmit={handleSubmit}>
+              <label>Email adress</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter email..."
+                value={inputData.email}
+                onChange={handleInput}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter password..."
+                value={inputData.password}
+                onChange={handleInput}
+              />
+              <button>Login</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
