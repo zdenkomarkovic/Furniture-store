02b3ac7c4ec5609922668deb22856e05b7dd9b65
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-24 20:39:57 +0100

    responsive

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -1,59 +1,64 @@
-import React from 'react';
-import './Register.scss';
-import { useState } from 'react';
-import UserService from '../../services/UserService';
-import Header from '../../Components/Header/Header';
-import { toast } from 'react-toastify';
+import React from "react";
+import "./Register.scss";
+import { useState } from "react";
+import UserService from "../../services/UserService";
+import Header from "../../Components/Header/Header";
+import { toast } from "react-toastify";
 
 const RegisterPage = () => {
   const [inputData, setInputData] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
   });
-  const handleInput = e => {
+  const handleInput = (e) => {
     setInputData({ ...inputData, [e.target.name]: e.target.value });
   };
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     UserService.register(inputData)
-      .then(res => {
+      .then((res) => {
         if (res.status === 201) {
           console.log(res.data.msg);
         }
-        toast.success('Check your email address for authentication link');
+        toast.success("Check your email address for authentication link");
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
   return (
     <div>
-      <Header title='Register' />
-      <div className='container'>
-        <div className='form-input'>
-          <form action='' onSubmit={handleSubmit}>
-            <input
-              type='text'
-              name='name'
-              placeholder='Enter name...'
-              value={inputData.name}
-              onChange={handleInput}
-            />
-            <input
-              type='email'
-              name='email'
-              placeholder='Enter email...'
-              value={inputData.email}
-              onChange={handleInput}
-            />
-            <input
-              type='password'
-              name='password'
-              placeholder='Enter password...'
-              value={inputData.password}
-              onChange={handleInput}
-            />
-            <button>Register</button>
-          </form>
+      <Header title="Register" />
+      <div className="register-wrapper">
+        <div className="container ">
+          <div className="form-input">
+            <form action="" onSubmit={handleSubmit}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter name..."
+                value={inputData.name}
+                onChange={handleInput}
+              />
+              <label>Email adress</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter email..."
+                value={inputData.email}
+                onChange={handleInput}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter password..."
+                value={inputData.password}
+                onChange={handleInput}
+              />
+              <button>Register</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
