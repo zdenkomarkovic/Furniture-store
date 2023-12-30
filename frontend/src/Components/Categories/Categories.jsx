3f3ffaf36877d55fdd0befea57ec364d3a1a5c9e
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2023-12-30 02:31:57 +0100

    slider

--- frontend/src/Components/Categories/Categories.jsx
+++ frontend/src/Components/Categories/Categories.jsx
@@ -1,13 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./Categories.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import CategoryService from "../../services/CategoryService";
-import { MdKeyboardDoubleArrowRight } from "react-icons/md";
-import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+
 import { setAllCategories } from "../../store/categorySlice";
-import { motion } from "framer-motion";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
   Navigation,
@@ -25,9 +22,6 @@ import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
 const Categories = () => {
-  const [width, setWidth] = useState(0);
-  const wrapper = useRef();
-  const [imageIndex, setImageIndex] = useState(0);
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.categoryStore);
 
@@ -41,38 +35,9 @@ const Categories = () => {
       });
   }, [categories]);
 
-  useEffect(() => {
-    setWidth(wrapper.current.scrollWidth - wrapper.current.offsetWidth);
-  }, []);
-
-  const handleShowNext = () => {};
-  const handleShowPrevious = () => {};
-
   return (
-    <div className="category-wrapper container">
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        ref={wrapper}
-        className="wrapper"
-        whileTap={{ cursor: "grabbing" }}
-      >
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-wrapper"
-        >
-          {categories.map((category, i) => {
-            return (
-              <motion.div key={i} className="category">
-                <h6>{category.title}</h6>
-                <img src={category.image} />
-              </motion.div>
-            );
-          })}
-        </motion.div>
-      </motion.div>
-      <div className="third-wrapper">
+    <div className="category-wrapper">
+      <div className="slider">
         <Swiper
           modules={[
             Navigation,
